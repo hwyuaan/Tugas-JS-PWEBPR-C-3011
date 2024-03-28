cfac8122a21d8d474bb6a25d14b1af435a0c3419
hwyuaann <yuanitatri126@gmail.com>
2024-03-28 15:11:54 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function klik1(){
+    document.getElementById("content3").classList.remove("hidden");
+    document.getElementById("content1").classList.add("hidden");
+}
+
+function klik2(){
+    document.getElementById("content2").classList.remove("hidden");
+    document.getElementById("content1").classList.add("hidden");
+}
